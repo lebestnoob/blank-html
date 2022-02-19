2cff425b8af51dfa172b4ede1a56dbf2f3a43d27
lebestnoob <lebestnoob12@gmail.com>
2022-02-18 20:02:49 -0800

    Add website icon to index

--- site.js
+++ site.js
@@ -1,18 +1,27 @@
 // Outdated methods to offer more compatibility with older browser.
 var domain = document.location.hostname;
+var encode = btoa(domain);
 var favicon = document.getElementById("favicon");
 var header = document.getElementById("site");
 var year = new Date().getFullYear();
 var copyright = document.getElementById("year");
-var footer = document.querySelector(".site");
+var footer = document.querySelector(".site"); // Classname wouldn't work for some odd reason.
+var icon = document.getElementById("icon");
+var dicebear = "https://avatars.dicebear.com/api/miniavs/" + encode + ".png";
 
 // Favicon
-var encode = btoa(domain);
-favicon.href = "https://avatars.dicebear.com/api/miniavs/" + encode + ".png";
+favicon.href = dicebear;
 
 // Refrences to domain
-header.innerText = domain;
+if (header) {
+    header.innerText = domain;
+}
 footer.innerHTML = domain;
 
-// Replace year
+// Year
 copyright.innerText = year;
+
+// Icon
+if (icon) {
+    icon.src = dicebear;
+}
