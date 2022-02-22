d9b0e2c6218f0aa21b433faf623247a8a74cc4f0
lebestnoob <lebestnoob12@gmail.com>
2022-02-21 16:59:25 -0800

    Use subdomain as site title instead of hostname

--- site.js
+++ site.js
@@ -5,12 +5,16 @@ var favicon = document.getElementById("favicon");
 var hostname = document.location.hostname;
 var encode_host = btoa(hostname);
 var path = window.location.pathname;
+var URL = document.location.protocol + "//" + document.location.host;
+var no_www = hostname.replace(/^(?:https?:\/\/)?(?:www\.)?/i, "").split("/")[0];
+var subdomain = no_www.split(".")[1] ? no_www.split(".")[0] : no_www;
+var site_name = subdomain.charAt(0).toUpperCase() + subdomain.slice(1);
 
 // Title truncate
-var title_hostname = hostname;
+var title_name = site_name;
 var truncate = 12;
-if (hostname.length > truncate)
-    title_hostname = hostname.substring(0, truncate) + "…";
+if (site_name.length > truncate)
+    title_name = site_name.substring(0, truncate) + "…";
 
 // Site strings:
 var year = new Date().getFullYear();
@@ -18,6 +22,7 @@ var header = document.getElementById("welcome");
 var copyright = document.getElementById("year");
 var footer = document.getElementById("footer-left");
 var icon = document.getElementById("icon");
+var link = document.createElement("a");
 var dicebear =
     "https://avatars.dicebear.com/api/miniavs/" + encode_host + ".png";
 
@@ -26,12 +31,16 @@ favicon.href = dicebear;
 
 // Title
 if (path == "/" || path == "/index" || path == "/index.html" || path == "")
-    document.title = "Home - " + title_hostname;
-else document.title = "404 - " + title_hostname;
+    document.title = "Home - " + title_name;
+else document.title = "404 - " + title_name;
 
-// Refrences to domain
-if (header) header.innerText = hostname;
-footer.innerHTML = hostname;
+// References to domain
+if (header) header.innerText = site_name;
+footer.innerText = "";
+// Footer link
+link.href = URL;
+link.textContent = hostname;
+footer.append(link);
 
 // Year
 copyright.innerText = year;
